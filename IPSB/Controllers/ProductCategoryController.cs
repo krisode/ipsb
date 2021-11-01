c50080cc9eea66edd23baf6905c9553702442658
krisode <mrsimple14062000@gmail.com>
2021-11-01 17:45:16 +0700

    HuyTTD_UPDATE_ProductCategoryController_1

--- IPSB/Controllers/ProductCategoryController.cs
+++ IPSB/Controllers/ProductCategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using IPSB.AuthorizationHandler;
 using IPSB.Core.Services;
+using IPSB.ExternalServices;
 using IPSB.Infrastructure.Contexts;
 using IPSB.Utils;
 using IPSB.ViewModels;
@@ -25,12 +26,15 @@ namespace IPSB.Controllers
         private readonly IProductCategoryService _service;
         private readonly IMapper _mapper;
         private readonly IPagingSupport<ProductCategory> _pagingSupport;
+        private readonly IUploadFileService _uploadFileService;
         // private readonly IAuthorizationService _authorizationService;
-        public ProductCategoryController(IProductCategoryService service, IMapper mapper, IPagingSupport<ProductCategory> pagingSupport)
+        public ProductCategoryController(IProductCategoryService service, IMapper mapper, 
+            IPagingSupport<ProductCategory> pagingSupport, IUploadFileService uploadFileService)
         {
             _service = service;
             _mapper = mapper;
             _pagingSupport = pagingSupport;
+            _uploadFileService = uploadFileService;
         }
 
 
@@ -86,7 +90,7 @@ namespace IPSB.Controllers
         ///     }
         ///
         /// </remarks>
-        /// <returns>All service types</returns>
+        /// <returns>All product categories</returns>
         /// <response code="200">Returns all product categories</response>
         /// <response code="404">No product categories found</response>
         [HttpGet]
@@ -96,14 +100,35 @@ namespace IPSB.Controllers
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<IEnumerable<ProductCategoryRefModel>> GetAllProductCategories([FromQuery] ProductCategorySM model, int pageSize = 20, int pageIndex = 1, bool isAll = false, bool isAscending = true)
         {
-            var serviceTypeList = _service.GetAll();
+            var list = _service.GetAll();
 
             if (!string.IsNullOrEmpty(model.Name))
             {
-                serviceTypeList = serviceTypeList.Where(_ => _.Name.Contains(model.Name));
+                list = list.Where(_ => _.Name.Contains(model.Name));
             }
 
-            var pagedModel = _pagingSupport.From(serviceTypeList)
+            if (!string.IsNullOrEmpty(model.Status))
+            {
+                if (model.Status != Constants.Status.ACTIVE && model.Status != Constants.Status.INACTIVE)
+                {
+                    return BadRequest();
+                }
+
+                else
+                {
+                    if (model.Status == Constants.Status.ACTIVE)
+                    {
+                        list = list.Where(_ => _.Status == Constants.Status.ACTIVE);
+                    }
+
+                    if (model.Status == Constants.Status.INACTIVE)
+                    {
+                        list = list.Where(_ => _.Status == Constants.Status.INACTIVE);
+                    }
+                }
+            }
+
+            var pagedModel = _pagingSupport.From(list)
                 .GetRange(pageIndex, pageSize, _ => _.Id, isAll, isAscending)
                 .Paginate<ProductCategoryRefModel>();
 
@@ -130,10 +155,8 @@ namespace IPSB.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<ActionResult<ProductCategoryCM>> CreateProductCategory([FromBody] ProductCategoryCM proCateModel)
         {
-            /*DateTime crtDate = DateTime.Now;
-            DateTime updDate = DateTime.Now;*/
 
-            ProductCategory crtProCateType = _mapper.Map<ProductCategory>(proCateModel);
+            ProductCategory crtProCate = _mapper.Map<ProductCategory>(proCateModel);
 
             /*var authorizedResult = await _authorizationService.AuthorizeAsync(User, crtProCateType, Operations.Create);
             if (!authorizedResult.Succeeded)
@@ -141,28 +164,30 @@ namespace IPSB.Controllers
                 return Forbid($"Not authorized to create product category: {proCateModel.Name}");
             }*/
 
+            string imageURL = await _uploadFileService.UploadFile("123456798", proCateModel.ImageUrl, "building", "building-detail");
+            crtProCate.ImageUrl = imageURL;
+
+            // Default status when creating is ACTIVE
+            crtProCate.Status = Constants.Status.ACTIVE;
+
             try
             {
-                /*crtService.CreatedDate = crtDate;
-                crtService.UpdatedDate = updDate;*/
-
-                await _service.AddAsync(crtProCateType);
+                await _service.AddAsync(crtProCate);
                 await _service.Save();
-
             }
             catch (Exception)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
 
-            return CreatedAtAction("GetProductCategoryById", new { id = crtProCateType.Id }, crtProCateType);
+            return CreatedAtAction("GetProductCategoryById", new { id = crtProCate.Id }, crtProCate);
         }
 
         /// <summary>
         /// Update product category with specified id
         /// </summary>
         /// <param name="id">Product category's id</param>
-        /// <param name="productCategory">Information applied to updated service type</param>
+        /// <param name="model">Information applied to updated service type</param>
         /// <response code="204">Update product category successfully</response>
         /// <response code="400">Product category's id does not exist or does not match with the id in parameter</response>
         /// <response code="500">Failed to update</response>
@@ -172,10 +197,11 @@ namespace IPSB.Controllers
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult> PutProductCategory(int id, [FromBody] ProductCategoryUM productCategory)
+        public async Task<ActionResult> PutProductCategory(int id, [FromBody] ProductCategoryUM model)
         {
             ProductCategory updProCate = await _service.GetByIdAsync(_ => _.Id == id);
-            if (updProCate == null || id != productCategory.Id)
+
+            if (updProCate == null || id != model.Id)
             {
                 return BadRequest();
             }
@@ -186,10 +212,18 @@ namespace IPSB.Controllers
             //     return Forbid($"Not authorized to update product category: {updProCate.Name}");
             // }
 
+            string imageURL = updProCate.ImageUrl;
+
+            if (model.ImageUrl is not null && model.ImageUrl.Length > 0)
+            {
+                imageURL = await _uploadFileService.UploadFile("123456798", model.ImageUrl, "product-category", "product-category-detail");
+            }
+
             try
             {
-                updProCate.Id = productCategory.Id;
-                updProCate.Name = productCategory.Name;
+                updProCate.Id = model.Id;
+                updProCate.Name = model.Name;
+                updProCate.ImageUrl = imageURL;
                 _service.Update(updProCate);
                 await _service.Save();
             }
@@ -201,14 +235,54 @@ namespace IPSB.Controllers
             return NoContent();
         }
 
-        // DELETE api/<ProductCategoryController>/5
-        // Change Status to Inactive
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        /// <summary>
+        /// Change the status of product category to inactive
+        /// </summary>
+        /// <param name="id">Product category's id</param>
+        /// <response code="204">Change product category's status successfully</response>
+        /// <response code="400">Product category's id does not exist</response>
+        /// <response code="500">Failed to delete</response>
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [HttpDelete]
+        [Route("{id}")]
+        [Produces("application/json")]
+        public async Task<ActionResult> Delete(int id)
         {
+            ProductCategory proCate = await _service.GetByIdAsync(_ => _.Id == id);
+
+            /*var authorizedResult = await _authorizationService.AuthorizeAsync(User, building, Operations.Delete);
+            if (!authorizedResult.Succeeded)
+            {
+                return new ObjectResult($"Not authorize to delete building with id: {id}") { StatusCode = 403 };
+            }*/
+
+            if (proCate is null)
+            {
+                return BadRequest();
+            }
+
+            if (proCate.Status.Equals(Constants.Status.INACTIVE))
+            {
+                return BadRequest();
+            }
+
+            proCate.Status = Constants.Status.INACTIVE;
+
+            try
+            {
+                _service.Update(proCate);
+                await _service.Save();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return NoContent();
 
         }
-
 
     }
 }
