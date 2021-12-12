1a6a50dd3aa44f97525c28b6f23d93046ba981c4
thehienvnag <thehienvnag@gmail.com>
2021-12-12 23:10:44 +0700

    HienHT/upload-file-controller

---
+++ IPSB/Controllers/UploadFileController.cs
@@ -0,0 +1,49 @@
+﻿using IPSB.ExternalServices;
+using IPSB.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IPSB.Controllers
+{
+    [Route("api/v1.0/upload-files")]
+    [ApiController]
+    public class UploadFileController : ControllerBase
+    {
+        private readonly IUploadFileService _uploadFileService;
+        public UploadFileController(IUploadFileService uploadFileService)
+        {
+            _uploadFileService = uploadFileService;
+        }
+
+        // POST api/<UploadFileController>
+        [HttpPost]
+        public async Task<ActionResult> PostFile([FromForm] UploadFileCM model)
+        {
+            if (model.File == null)
+            {
+                return BadRequest();
+            }
+            string imageUrl = null;
+
+            try
+            {
+                imageUrl = await _uploadFileService.UploadFile("123456798", model.File, "temp", "temp-files");
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            if (imageUrl != null)
+            {
+                return Ok(imageUrl);
+            }
+            return NotFound(imageUrl);
+        }
+    }
+}
